f4917c55368d514544ae707437cf7b30ed7425f4
Jagan0901 <Jaganath0901@gmail.com>
2023-05-11 19:00:15 +0530

    err fixed

--- src/App.js
+++ src/App.js
@@ -5,45 +5,46 @@ import { useEffect, useState } from "react";
 import { API } from "./api";
 import { InputField } from "./Components/InputField";
 
-const API_KEY = "31ps09wcojiett784rccja9cunqsg8qthxdewa5v74thr1p61lirmstev9lh"; //upto 50 requests per month
-const API_KEY1 = "0mgdrfjgfp9vxlgvzabek0l0xy16uv6d2umx6he7oz419u522d32h12oa9hk";//upto 50 requests per month
-const API_KEY2 = "nefnopne08vnns2ks1soj86l4h7hi5x38329oudn4d4nrz0i3hvtve02hw6x";//upto 50 requests per month
-const API_KEY3 = "bx6jdfcgc1ubckba1bvskz988d369sj388hc9yb7s0br3n788x7m1sm4u60d";//upto 50 requests per month
-const API_KEY4 = "ok30wg8a7t93tas7i3252b2308xwxjl6an091kbfhe7v2t8mj8sf7xughbzj";//upto 50 requests per month
+// const API_KEY = "31ps09wcojiett784rccja9cunqsg8qthxdewa5v74thr1p61lirmstev9lh"; //upto 50 requests per month
+// const API_KEY1 = "0mgdrfjgfp9vxlgvzabek0l0xy16uv6d2umx6he7oz419u522d32h12oa9hk";//upto 50 requests per month
+// const API_KEY2 = "nefnopne08vnns2ks1soj86l4h7hi5x38329oudn4d4nrz0i3hvtve02hw6x";//upto 50 requests per month
+// const API_KEY3 = "bx6jdfcgc1ubckba1bvskz988d369sj388hc9yb7s0br3n788x7m1sm4u60d";//upto 50 requests per month
+// const API_KEY4 = "ok30wg8a7t93tas7i3252b2308xwxjl6an091kbfhe7v2t8mj8sf7xughbzj";//upto 50 requests per month
 
 function App() {
-  const [goldData, setGoldData] = useState({});
+  const [goldData, setGoldData] = useState(null);
 
   //To Get gold price data from Public API
   const getGoldPrice = () => {
-    fetch(
-      `https://metals-api.com/api/latest?access_key=${API_KEY4}&base=INR&symbols=XAU`,
-      { method: "GET" }
+    const URL = "https://www.goldapi.io/api/XAU/INR";
+    // const url = `https://metals-api.com/api/latest?access_key=${API_KEY2}&base=INR&symbols=XAU`
+    fetch(URL,
+      { method  : "GET",
+        headers : {"x-access-token": "goldapi-1lh6prlhizyzn5-io", "Content-Type" : "application/json"} 
+      }
     )
       .then((response) => response.json())
-      .then((data) => {
-        if (data.success === true) {
-          return setGoldData(data);
-        }
-      });
+      .then((data) => setGoldData(data))
+      .catch((error) => console.log('error:', error))
   };
 
   //Inserting gold price data in Database
   const postGoldData = () => {
-    if (goldData.success === true) {
+    const timeStampData = Date.now();
+    const timeStamp = new Date(timeStampData);
+    const date = timeStamp.toISOString().split('T')[0]
       const gold = {
-        date: goldData.date,
-        price: goldData.rates.XAU,
+        date: date,
+        price: goldData.price,
       };
       fetch(`${API}/Gold/post`, {
         method: "POST",
         body: JSON.stringify(gold),
         headers: { "Content-Type": "application/json", "x-auth-token" : "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0MjFjNjBkM2RkN2Q3NWI1NTU4ZmZlZSIsImlhdCI6MTY3OTkzNjIxNX0.DmH3UmlTN8sdie9eP_HUQ_sDA2KgkMLXy9egMFMkGFo" },
       }).then((data) => data.json());
-    }
   };
 
-  postGoldData()
+  if(goldData !==null) postGoldData();
 
   useEffect(() => getGoldPrice(), []);
 

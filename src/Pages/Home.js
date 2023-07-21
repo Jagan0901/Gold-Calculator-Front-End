02a87294eea08eb6b46661c84e83a5b3d6ba8375
Jagan0901 <Jaganath0901@gmail.com>
2023-07-21 19:35:59 +0530

    added till login and signUp

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -34,7 +34,6 @@ export const Home = () => {
 
      //To Get gold price data from Public API
      const getGoldPrice = () => {
-       const fetchData = () => {
          const URL = "https://www.goldapi.io/api/XAU/INR";
          fetch(URL, {
            method: "GET",
@@ -46,12 +45,6 @@ export const Home = () => {
            .then((response) => response.json())
            .then((data) => setGoldData(data))
            .catch((error) => console.log("error:", error));
-       };
-       fetchData(); //Initial API call when the component mount
-
-       const interval = setInterval(fetchData, 12 * 60 * 60 * 1000);
-
-       return () => clearInterval(interval);
      };
 
      //Inserting gold price data in Database
